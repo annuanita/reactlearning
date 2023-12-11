be529dc1169fb2ab47e01968f548b051b3b93f3d
Anita <anitamaurya12353@gmail.com>
2023-12-11 18:46:59 +0530

    first

---
+++ react.js
@@ -0,0 +1,7 @@
+//const heading = React.createElement("h1" , {id: "heading" ,xyz: "abc"} ,"hello from react");
+ 
+const parent =React.createElement("h1" , {id: "parent"} ,
+[React.createElement("h2" ,{id:"child"}, "hi child from react"),React.createElement("h2",{id: "child2"},"second child in react")]);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+   // root.render(heading);
+    root.render(parent);
