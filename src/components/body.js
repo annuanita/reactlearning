f91885d3f234a9422e9440f9cafde15e7647a7cc
Anita <anitamaurya12353@gmail.com>
2024-01-04 22:21:57 +0530

    search bar update

--- src/components/body.js
+++ src/components/body.js
@@ -1,22 +1,29 @@
 import Resturantcard from "./Resturantcard";
 import resobj from "../utils/constants";
+import { useState } from "react";
+
+
+
 const Body =() =>{
+
+    const [listres,setlistres] = useState(resobj);
     return(
         <div className="body-container">
-            <div className="search-bar">search</div>
+            <div className="search-bar">
+                <button className="button-filter"
+                onClick={()=>{
+                    const restfilter = listres.filter(
+                        (res) => res.data.avgRating > 4
+                    );
+                    setlistres(restfilter);
+                } }> 
+                Top rated resturants  
+                </button>
+              </div>
             <div className="resturant-card">
-            <Resturantcard restdata={resobj[0]}/>
-            <Resturantcard restdata={resobj[1]}/>
-            <Resturantcard restdata={resobj[2]}/>
-            <Resturantcard restdata={resobj[3]}/>
-            <Resturantcard restdata={resobj[4]}/>
-            <Resturantcard restdata={resobj[5]}/>    
-            <Resturantcard restdata={resobj[0]}/>
-            <Resturantcard restdata={resobj[1]}/>
-            <Resturantcard restdata={resobj[2]}/>
-            <Resturantcard restdata={resobj[3]}/>
-            <Resturantcard restdata={resobj[4]}/>
-            <Resturantcard restdata={resobj[5]}/>
+               { listres.map((resturant) =>(
+                <Resturantcard key = {resturant.data.id} restdata={resturant}/>
+                 ))}
             </div>
         </div>
 
